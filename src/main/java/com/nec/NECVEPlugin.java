271e4087126081ec93014dccc565770e34171696
tangzhankun <tangzhankun@gmail.com>
2018-11-01 17:12:25 +0800

    add a test main class

--- src/main/java/com/nec/NECVEPlugin.java
+++ src/main/java/com/nec/NECVEPlugin.java
@@ -3,29 +3,38 @@ package com.nec;
 
 import org.apache.hadoop.util.Shell;
 import org.apache.hadoop.yarn.server.nodemanager.api.deviceplugin.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 import java.util.TreeSet;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class NECVEPlugin implements DevicePlugin, DevicePluginScheduler {
+
   public static final Logger LOG = LoggerFactory.getLogger(NECVEPlugin.class);
 
   private static final String[] DEFAULT_BINARY_SEARCH_DIRS = new String[]{
-      "/usr/bin", "/bin", "/opt/nec/ve/bin", "~/"};
+      "/usr/bin", "/bin", "/opt/nec/ve/bin"};
 
   private String binaryName = "veGet.py";
+  private String binaryPath;
   private File binaryFile;
 
   public NECVEPlugin() throws Exception {
-    String binaryName = System.getenv("NEC_VE_GET_SCRIPT_NAME");
-    if (null != binaryName) {
-      LOG.info("Use {} as script name.", binaryName);
-      this.binaryName = binaryName;
+    String envBinaryName = System.getenv("NEC_VE_GET_SCRIPT_NAME");
+    if (null != envBinaryName) {
+      LOG.info("Use " + binaryName + "as script name.");
+      this.binaryName = envBinaryName;
     }
+    String envBinaryPath = System.getenv("NEC_VE_GET_SCRIPT_PATH");
+    if (null != envBinaryPath) {
+      this.binaryPath = envBinaryPath;
+      LOG.info("Use script: " + binaryPath);
+      return;
+    }
+    LOG.info("Search script..");
     // search binary exists
     boolean found = false;
     for (String dir : DEFAULT_BINARY_SEARCH_DIRS) {
@@ -33,13 +42,13 @@ public class NECVEPlugin implements DevicePlugin, DevicePluginScheduler {
       if (binaryFile.exists()) {
         found = true;
         this.binaryName = binaryFile.getAbsolutePath();
-        LOG.info("Found script \"{}\"", this.binaryName);
+        LOG.info("Found script:" + this.binaryName);
         break;
       }
     }
     if (!found) {
-      LOG.error("No \"{}\" found in below path \"{}\"",
-          DEFAULT_BINARY_SEARCH_DIRS);
+      LOG.error("No binary found in below path"
+          + DEFAULT_BINARY_SEARCH_DIRS.toString());
       throw new Exception("No binary found for " + NECVEPlugin.class);
     }
 
@@ -55,7 +64,7 @@ public class NECVEPlugin implements DevicePlugin, DevicePluginScheduler {
 
     String output = null;
     Shell.ShellCommandExecutor shexec = new Shell.ShellCommandExecutor(
-        new String[]{binaryName});
+        new String[]{this.binaryPath});
     try {
       shexec.execute();
       output = shexec.getOutput();
@@ -85,16 +94,18 @@ public class NECVEPlugin implements DevicePlugin, DevicePluginScheduler {
    * id=0, dev=/dev/ve0, state=ONLINE, busId=0000:65:00.0, major=243, minor=0
    */
   private void parseOutput(TreeSet<Device> r, String output) {
+    LOG.info("Parsing output: {}", output);
     String[] lines = output.split("\n");
     for (String line : lines) {
+      Device.Builder builder = Device.Builder.newInstance();
       String[] keyvalues = line.trim().split(",");
       for (String keyvalue : keyvalues) {
         String[] tokens = keyvalue.trim().split("=");
-        Device.Builder builder = Device.Builder.newInstance();
         if (tokens.length != 2) {
           LOG.error("Unknown format of script output! Skip this line");
           break;
         }
+
         if (tokens[0].equals("id")) {
           builder.setID(Integer.valueOf(tokens[1]));
         }
@@ -114,8 +125,8 @@ public class NECVEPlugin implements DevicePlugin, DevicePluginScheduler {
         if (tokens[0].equals("minor")) {
           builder.setMinorNumber(Integer.valueOf(tokens[1]));
         }
-        r.add(builder.build());
       }// for key value pars
+      r.add(builder.build());
     }
   }
 
@@ -139,13 +150,5 @@ public class NECVEPlugin implements DevicePlugin, DevicePluginScheduler {
       }
     }
     return allocated;
-  }
-
-  public static void main(String[] args) throws Exception {
-    NECVEPlugin plugin = new NECVEPlugin();
-    Set<Device> r = plugin.getDevices();
-    for (Device device : r) {
-      System.out.println(device);
-    }
   }
 }
